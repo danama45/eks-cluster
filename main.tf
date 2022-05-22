6b6b6f0e8c7d17b3c1ad53f28ad5a29ce2aaffe9
Alemmor <Alemmor82@yahoo.co.uk>
2022-05-22 15:13:53 -0600

    Added DNS hostname and support

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "networking" {
-  source   = "./modules/networking"
-  vpc_cidr = var.vpc_cidr
-  vpc_name = var.vpc_name
-  igw_name = var.igw_name
+  source           = "./modules/networking"
+  vpc_cidr         = var.vpc_cidr
+  vpc_name         = var.vpc_name
+  igw_name         = var.igw_name
   eks_cluster_name = var.eks_cluster_name
 }
--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,18 +1,20 @@
 # Local variables
 locals {
   private_subnet_name = "private_subnet"
-  public_subnet_name = "public_subnet"
+  public_subnet_name  = "public_subnet"
 }
 
 # VPC for EKS cluster
 resource "aws_vpc" "eks" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
+  enable_dns_hostnames = true
+  enable_dns_support = true
 
   tags = {
     "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
-    Name = var.vpc_name
-    Env  = "Prod"
+    Name                                            = var.vpc_name
+    Env                                             = "Prod"
   }
 }
 
@@ -27,38 +29,38 @@ resource "aws_internet_gateway" "eks_igw" {
 
 # Private subnet for EKS VPC
 resource "aws_subnet" "eks_private_subnet" {
-  count = length(var.private_cidr_block)
-  vpc_id     = aws_vpc.eks.id
-  cidr_block = element(var.private_cidr_block,count.index)
-  availability_zone = element(var.private_az,count.index)
-  
+  count             = length(var.private_cidr_block)
+  vpc_id            = aws_vpc.eks.id
+  cidr_block        = element(var.private_cidr_block, count.index)
+  availability_zone = element(var.private_az, count.index)
+
   tags = {
     "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb" = 1
-    Name = local.private_subnet_name
+    "kubernetes.io/role/internal-elb"               = 1
+    Name                                            = local.private_subnet_name
   }
 }
 
 # Public subnet for EKS VPC
 resource "aws_subnet" "eks_public_subnet" {
-  count = length(var.public_cidr_block)
-  vpc_id     = aws_vpc.eks.id
-  cidr_block = element(var.public_cidr_block,count.index)
-  availability_zone = element(var.public_az,count.index)
+  count             = length(var.public_cidr_block)
+  vpc_id            = aws_vpc.eks.id
+  cidr_block        = element(var.public_cidr_block, count.index)
+  availability_zone = element(var.public_az, count.index)
 
   tags = {
     "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
-    "kubernetes.io/role/elb" = 1
-    Name = local.public_subnet_name
+    "kubernetes.io/role/elb"                        = 1
+    Name                                            = local.public_subnet_name
   }
 }
 
 resource "aws_eip" "nat_gateway" {
-  vpc      = true
+  vpc = true
 }
 resource "aws_nat_gateway" "ngw" {
   allocation_id = aws_eip.nat_gateway.id
-  subnet_id  = aws_subnet.eks_public_subnet[0].id
+  subnet_id     = aws_subnet.eks_public_subnet[0].id
 
   tags = {
     Name = "NAT Gateway"
@@ -83,7 +85,7 @@ resource "aws_route_table" "public_rtb" {
 }
 
 resource "aws_route_table_association" "pub_subnets" {
-  count = length(var.public_cidr_block)
+  count          = length(var.public_cidr_block)
   subnet_id      = element(aws_subnet.eks_public_subnet.*.id, count.index)
   route_table_id = aws_route_table.public_rtb.id
 }
@@ -92,7 +94,7 @@ resource "aws_route_table" "private_rtb" {
   vpc_id = aws_vpc.eks.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
@@ -102,7 +104,7 @@ resource "aws_route_table" "private_rtb" {
 }
 
 resource "aws_route_table_association" "private_subnets" {
-  count = length(var.private_cidr_block)
+  count          = length(var.private_cidr_block)
   subnet_id      = element(aws_subnet.eks_private_subnet.*.id, count.index)
   route_table_id = aws_route_table.private_rtb.id
 }
